590be2ed825e41feb3d5dae9a990ef0913bf7560
Glanms <guojq0ms@gmail.com>
2015-11-18 13:40:41 +0800

    简单加入设置应用不同icon的方法

--- app/src/main/java/com/base/list/libsgisk/MainActivity.java
+++ app/src/main/java/com/base/list/libsgisk/MainActivity.java
@@ -1,10 +1,23 @@
 package com.base.list.libsgisk;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +25,8 @@ import android.widget.Button;
 
 import com.base.list.libsgisk.activity.LanguageConfigActivity;
 import com.base.list.libsgisk.tools.CommonUtils;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -44,6 +59,123 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
         }
+    }
+
+
+
+    //新增快捷方式
+    private void shortcutAdd(String name, int number) {
+        //设置快捷方式点击后要打开的Activity（主入口）
+        Intent shortcutIntent = new Intent(getApplicationContext(), MainActivity.class);
+        shortcutIntent.setAction(Constant.ACTION_PLAY);
+
+        //这里创建了一个number的bitmap, 也可以设置自己想要的图表
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.icon_new);
+//        Paint paint = new Paint();
+//        paint.setColor(0xFF808080); // gray
+//        paint.setTextAlign(Paint.Align.CENTER);
+//        paint.setTextSize(50);
+//        new Canvas(bitmap).drawText("" + number, 50, 50, paint);
+
+        //设置快捷方式
+        Intent addIntent = new Intent();
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, bitmap);
+
+        //创建快捷方式
+        addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        getApplicationContext().sendBroadcast(addIntent);
+    }
+
+    //删除快捷方式
+    private void shortcutDel(String name) {
+        // Intent to be send, when shortcut is pressed by user ("launched")
+        Intent shortcutIntent = new Intent(getApplicationContext(), MainActivity.class);
+        shortcutIntent.setAction(Constant.ACTION_PLAY);
+
+        // Decorate the shortcut
+        Intent delIntent = new Intent();
+        delIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        delIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+
+        // Inform launcher to remove shortcut
+        //删除快捷方式
+        delIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
+        getApplicationContext().sendBroadcast(delIntent);
+    }
+
+    //判断快捷方式是否存在
+    public boolean isAddShortCut() {
+
+        final ContentResolver cr = this.getContentResolver();
+
+        int versionLevel = android.os.Build.VERSION.SDK_INT;
+        String AUTHORITY = "com.android.launcher2.settings";
+
+        //2.2以上的系统的文件文件名字是不一样的
+        if (versionLevel >= 8) {
+            AUTHORITY = "com.android.launcher2.settings";
+        } else {
+            AUTHORITY = "com.android.launcher.settings";
+        }
+
+        final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
+                + "/favorites?notify=true");
+        Cursor c = cr.query(CONTENT_URI,
+                new String[]{"title", "iconResource"}, "title=?",
+                new String[]{getString(R.string.app_name)}, null);
+
+        if (c != null && c.getCount() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private void setAppIcon(boolean iconId){
+        Context ctx = getApplicationContext();
+        PackageManager pm = getPackageManager();
+        ActivityManager am = (ActivityManager) ctx
+                .getSystemService(Activity.ACTIVITY_SERVICE);
+        Log.d("TAG:", "PackageName--->" + getComponentName());
+        if(iconId) {
+            if ("com.gisk.list.myapplication.MainActivity_Version1".equals(getComponentName()
+                    .getClassName())) {
+                pm.setComponentEnabledSetting(getComponentName(),
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP);
+                pm.setComponentEnabledSetting(new ComponentName(getBaseContext(),
+                                "com.gisk.list.myapplication.MainActivity_Version2"),
+                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP);
+            } else {
+                pm.setComponentEnabledSetting(getComponentName(),
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP);
+                pm.setComponentEnabledSetting(new ComponentName(getBaseContext(),
+                                "com.gisk.list.myapplication.MainActivity_Version1"),
+                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP);
+            }
+        }else {
+            Log.d("TAG","类型 2");
+        }
+
+        // Find launcher and kill it
+        Intent i = new Intent(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_HOME);
+        i.addCategory(Intent.CATEGORY_DEFAULT);
+        List<ResolveInfo> resolves = pm.queryIntentActivities(i,0);
+        for(ResolveInfo res : resolves){
+            if(res.activityInfo != null){
+//                am.killBackgroundProcesses(res.activityInfo.packageName);
+                am.restartPackage(res.activityInfo.packageName);
+            }
+        }
+
+
+        // Change ActionBar icon
+//        getActivity().getActionBar().setIcon(iconId);
     }
 
 
---
+++ app/src/main/java/com/base/list/libsgisk/entity/Constant.java
@@ -0,0 +1,9 @@
+package com.base.list.libsgisk.entity;
+
+/**
+ * Created by Administrator on 2015/11/18.
+ */
+public class Constant  {
+
+    public static final String ACTION_PLAY = "android.intent.action.MAIN";
+}
